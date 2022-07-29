96cbbb89806b5f1aeec29a7b37543a390a411864
Derek Vogt <derekvogt3@gmail.com>
2022-07-29 17:00:07 -0400

    initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import Pixel from "../components/PixelArt";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>home page</h1>
+      <Pixel></Pixel>
+      <h2>test</h2>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/TicTacToe.js
@@ -0,0 +1,9 @@
+function TicTacToe() {
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+    </div>
+  );
+}
+
+export default TicTacToe;
