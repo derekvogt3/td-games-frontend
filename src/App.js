4e0c324d330c6acf3883658caf1238c3c7c16a41
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-07-29 22:51:24 -0400

    added the login form and logic

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import "./App.css";
 import TicTacToe from "./pages/TicTacToe";
 
 import ReactDOM from "react-dom/client";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState([])
+  const [users, setUsers] = useState([])
+
+  const usersList = {}
+  if (users.length > 0) {
+    users.forEach(user => {
+      usersList[user.username] = user.password
+    })
+  }
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/users`)
+    .then(res => res.json())
+    .then(setUsers)
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/" element={<HomePage users={users} usersList={usersList} currentUser={currentUser} setCurrentUser={setCurrentUser} />}></Route>
         <Route path="/tictactoe" element={<TicTacToe />}></Route>
       </Routes>
     </BrowserRouter>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import Pixel from "../components/PixelArt";
+import LoginForm from "./LoginForm";
 
-function HomePage() {
+function HomePage({users, usersList, currentUser, setCurrentUser}) {
   return (
     <div>
       <h1>home page</h1>
+      <LoginForm users={users} usersList={usersList} currentUser={currentUser} setCurrentUser={setCurrentUser} />
       <Pixel></Pixel>
       <h2>test</h2>
     </div>
