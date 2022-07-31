a9600fe1c70835236c5d143ac6fbea4e63b3c992
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-07-31 17:24:48 -0400

    can update message now

--- src/pages/messages/Message.js
+++ src/pages/messages/Message.js
@@ -5,6 +5,8 @@ import "./Message.css"
 function MyMessage({currentUser, message}) {
   const [sender, setSender] = useState({})
   const [style, setStyle] = useState({flexDirection: "row"})
+  const [editMode, setEditMode] = useState(false)
+  const [editFromInput, setEditFromInput] = useState(message.message)
 
   const Img = sender.profile_img ? sender.profile_img : "https://wellbeingchirony.com/wp-content/uploads/2021/03/Deafult-Profile-Pitcher.png"
   const online = sender.is_login ? {backgroundColor: "green"} : {backgroundColor: "red"}
@@ -23,6 +25,23 @@ function MyMessage({currentUser, message}) {
     }
   }, [sender])
 
+  function editMessage(e) {
+    e.preventDefault()
+    fetch(`${fetchUrl}/messages/${message.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        message: editFromInput
+      })
+    })
+    .then(res => res.json())
+    .then(() => setEditMode(false))
+    .catch(console.error)
+  }
+
   return (
     <>
       {
@@ -36,11 +55,40 @@ function MyMessage({currentUser, message}) {
               <div className="message-info" style={style}>
                 <p className="message-sender">{sender.username.slice(0, 1).toUpperCase()}{sender.username.slice(1)}</p>
                 <p className="message-time">{message.created_at.slice(0,10)} {message.created_at.slice(11,19)}</p>
+                {
+                  message.created_at != message.updated_at ? (
+                    <p className="message-edited-tag">Edited</p>
+                  ) : (
+                    null
+                  )
+                }
               </div>
               <div className="message">
                 <p>{message.message}</p>
               </div>
             </div>
+            {
+              currentUser.id == sender.id ? (
+                <div className="edit-mode">
+                  <img className="message-edit-btn" src="https://img.icons8.com/ios/50/000000/edit-chat-history.png" alt="edit" onClick={() => setEditMode(!editMode)}/>
+                  <img className="message-recall-btn" src="https://img.icons8.com/external-solid-style-bomsymbols-/65/000000/external-bin-business-shop-finance-solid-style-set-2-solid-style-bomsymbols-.png" alt="recall"/>
+                  {
+                    editMode ? (
+                      <form className="message-edit-form" onSubmit={editMessage}>
+                        <input type="text" value={editFromInput} onChange={e => setEditFromInput(e.target.value)} />
+                        <button type="submit">
+                          <img src="https://img.icons8.com/ios/50/000000/edit-chat-history.png" alt="edit"/>
+                        </button>
+                      </form>
+                    ) : (
+                      null
+                    )
+                  }
+                </div>
+              ) : (
+                null
+              )
+            }
           </div>
         ) : (
           null
