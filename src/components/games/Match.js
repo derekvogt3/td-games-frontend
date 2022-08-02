d59f5b62e5c365f8d539d67d3a72ba7dc2311a41
Derek Vogt <derekvogt3@gmail.com>
2022-08-02 10:15:24 -0400

    added additional functionality

--- src/components/games/Match.js
+++ src/components/games/Match.js
@@ -1,6 +1,37 @@
 import React from "react";
+import "./Match.css";
+import { fetchUrl } from "../../utilities/GlobalVariables";
 
 export default function Match({ usermatch, friend, currentUser }) {
+  function handleAccept() {
+    let obj = { match_id: usermatch.match_id };
+
+    fetch(`${fetchUrl}/accept_invite`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
+  function handleReject() {
+    let obj = { match_id: usermatch.match_id };
+    fetch(`${fetchUrl}/reject_invite`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
   const Img = friend.profile_img
     ? friend.profile_img
     : "https://wellbeingchirony.com/wp-content/uploads/2021/03/Deafult-Profile-Pitcher.png";
@@ -19,13 +50,42 @@ export default function Match({ usermatch, friend, currentUser }) {
           {friend.username.slice(1)}
         </p>
       </div>
-      <div className="friend-name">
-        <p>
-          <i>
-            {usermatch.status.slice(0, 1).toUpperCase()}
-            {usermatch.status.slice(1)}
-          </i>
-        </p>
+
+      <div className="match-row">
+        {usermatch.status === "pending" ? (
+          <>
+            {usermatch.invited_by === currentUser.id ? (
+              <p>
+                <i>
+                  {usermatch.status.slice(0, 1).toUpperCase()}
+                  {usermatch.status.slice(1)}
+                </i>
+              </p>
+            ) : (
+              <div className="button-group-ar">
+                <button className="button-68" onClick={handleAccept}>
+                  accept
+                </button>
+                <button className="button-69" onClick={handleReject}>
+                  reject
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <>
+            {usermatch.status == "in match" ? (
+              <button className="button-70">Go to Match</button>
+            ) : (
+              <p>
+                <i>
+                  {usermatch.status.slice(0, 1).toUpperCase()}
+                  {usermatch.status.slice(1)}
+                </i>
+              </p>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
